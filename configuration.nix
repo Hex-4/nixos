23fad28ebb38fbc9fca13b5b5b6be40b524f0f30
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-04-08 19:52:34 -0600

    Hopefully fix FAH with an amd gpu

---
+++ configuration.nix
@@ -0,0 +1,220 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.theme = /home/hexatron/code/catppuccin/grub/src/catppuccin-mocha-grub-theme;
+
+  # gpu stuff 
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  #  Enable Flakes: Lock your sytem to a nixpkgs commit, and be able to add multiple sources
+  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  
+  #   Flathub
+  services.flatpak.enable = true;
+
+  # 󰞉 Enable networking
+  networking.networkmanager.enable = true;
+
+  # 󰂯 Bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  # 󰔠 Set your time zone.
+  time.timeZone = "America/Edmonton";
+
+  # 󰇧 Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_CA.UTF-8";
+    LC_IDENTIFICATION = "en_CA.UTF-8";
+    LC_MEASUREMENT = "en_CA.UTF-8";
+    LC_MONETARY = "en_CA.UTF-8";
+    LC_NAME = "en_CA.UTF-8";
+    LC_NUMERIC = "en_CA.UTF-8";
+    LC_PAPER = "en_CA.UTF-8";
+    LC_TELEPHONE = "en_CA.UTF-8";
+    LC_TIME = "en_CA.UTF-8";
+  };
+
+  #   Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  # 󰇀 Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  #  Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # 󰋋 Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # 󰈺 Add fish 
+  programs.fish.enable=true;
+
+  # 󱃲 Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hexatron = {
+    isNormalUser = true;
+    description = "Hex Four";
+    shell = pkgs.fish;
+    extraGroups = [ "networkmanager" "wheel" "hexatron" "dialout"];
+    packages = with pkgs; [
+      firefox
+      kate
+      gum
+      kitty
+      lmms
+    #  thunderbird
+    ];
+  };
+
+  # 🏠 Set up home-manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    useGlobalPkgs = true;
+    users = {
+      "hexatron" = import ./home.nix;
+    };
+  };
+
+  #  Allow generic linux executables
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages:
+  ];
+
+  #  Best WM
+  programs.hyprland.enable = true;
+  
+  # 󰗦 Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+  ];
+
+  fonts.fontDir.enable = true;
+
+  # Folding@home
+  services.foldingathome = {
+    enable = true;
+    team = 234980;
+    user = "6pcwsx2o3xc3";
+  };
+
+  # 󰏗 List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    (pkgs.appimageTools.wrapType2 {
+      name = "plover";
+      src = pkgs.fetchurl {
+        url = "https://github.com/openstenoproject/plover/releases/download/continuous/plover-4.0.0rc2+6.g53c416f-x86_64.AppImage";
+        hash = "sha256-aoqytTczT3FWr2yOuC0Ai7nCD22Y23OGLNFfgI/fg7s=";
+      };
+      extraPkgs = pkgs: with pkgs; [ ];
+    })
+    gccgo13
+    openrgb-with-all-plugins
+    ocl-icd
+  ];
+
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    setLdLibraryPath = true;
+    extraPackages = with pkgs; [
+      rocm-opencl-icd
+      rocm-opencl-runtime
+      amdvlk
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+# Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+     enable = true;
+     enableSSHSupport = true;
+   };
+
+  # List services that you want to enable:
+
+  # 󰣀
+   services.openssh.enable = true;
+
+  # OpenRGB (with plugins? maybe? idk man)
+  services.hardware.openrgb.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
---
+++ home.nix
@@ -0,0 +1,118 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "hexatron";
+  home.homeDirectory = "/home/hexatron";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # 󰏖 The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    pkgs.hello
+
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.discord
+    pkgs.discordo
+    pkgs.vesktop
+    pkgs.floorp
+    pkgs.starship
+    pkgs.fish
+    pkgs.neovim
+    pkgs.nodejs_21
+    pkgs.unzipNLS
+    pkgs.python3
+    pkgs.libgcc
+    pkgs.neofetch
+    pkgs.atuin
+    pkgs.btop
+    pkgs.bat
+    pkgs.eza
+    pkgs.tldr
+    pkgs.tmux
+    (pkgs.appimageTools.wrapType2 { # or wrapType1
+      name = "osu";
+      src = pkgs.fetchurl {
+        url = "https://github.com/ppy/osu/releases/latest/download/osu.AppImage";
+        hash = "sha256-1dzgs1p3/pf4eCdKvQ9JxowN+oBPBNaZv5e6qHeFPEM=";
+      };
+      extraPkgs = pkgs: with pkgs; [
+        icu
+      ];
+    })
+    (pkgs.appimageTools.wrapType2 { # or wrapType1
+      name = "obsidian";
+      src = pkgs.fetchurl {
+        url = "https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/Obsidian-1.5.12.AppImage";
+        hash = "sha256-qmXZmSp7YPZ2k2+8gNYW9Fz5s0aMSrYHMBI7cn9M8u4=";
+     };
+    extraPkgs = pkgs: with pkgs; [ ];
+    })
+    pkgs.aseprite
+    pkgs.gh
+    pkgs.safeeyes
+    pkgs.flyctl
+    pkgs.godot_4
+    pkgs.krita
+
+    # Hyprland stuff
+    pkgs.dunst
+    pkgs.waybar
+    pkgs.wttrbar
+    pkgs.wofi
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/hexatron/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
